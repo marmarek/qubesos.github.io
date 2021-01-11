ce1d27650c7b68d3c6a09dfa9514170477ea8369
dd <d@d.com>
2021-01-11 08:49:29 +0100

    testing workflow

--- _utils/_translation_utils/post_transifex_pull.sh
+++ _utils/_translation_utils/post_transifex_pull.sh
@@ -14,12 +14,12 @@ echo "============================ post processing step 2 ======================
 ruby _utils/_translation_utils/merge_md_heading_ids.rb $1 /tmp/tx-mapping
 
 echo "============================ post processing step 3 press to cont ======================================"
-read b
+#read b
 python3 _utils/_translation_utils/postprocess_translation.py $1 $2  /tmp/tx-mapping  /tmp/translated_href_urls.txt --yml
 
 
 echo "============================ post processing step 4 press to cont ======================================"
-read b
+#read b
 bash _utils/_translation_utils/postprocess_translation.sh $1 $2 /tmp/translated_href_urls.txt
 
 
